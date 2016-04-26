714a372801eef4ed60712d39c42828295e4c30bb
Nic Jackson <nicj@kewm1496.local>
2016-04-26 21:07:20 +0100

    added comment

--- lib/generators/template.rb
+++ lib/generators/template.rb
@@ -6,9 +6,10 @@ require 'minke/generators/config'
 module Minke
   module Generators
     module Template
+      # Register the template with minke
       config = Minke::Generators::Config.new
       config.name = 'minke-generator-template'
-      config.template_location = File.expand_path(File.dirname(__FILE__)) + '/scaffold'
+      config.template_location = File.expand_path(File.dirname(__FILE__)) + '/template/scaffold'
 
       Minke::Generators.register config
     end
