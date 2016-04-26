71ddd45b072934dfab7415167b19113ff67d696e
Nic Jackson <nicj@kewm1496.local>
2016-04-26 14:32:17 +0100

    initial checkin

---
+++ lib/minke/generator/template.rb
@@ -0,0 +1,9 @@
+require "minke/generator/template/version"
+
+module Minke
+  module Generator
+    module Template
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/minke/generator/template/version.rb
@@ -0,0 +1,7 @@
+module Minke
+  module Generator
+    module Template
+      VERSION = "0.1.0"
+    end
+  end
+end
